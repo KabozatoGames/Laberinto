4c970f5c98e89d9ae47702621ac66f2b1268c876
KamiloBarrera <kamilo_252@hotmail.com>
2017-07-23 21:43:14 -0400

    Update Juego2.py
    Linea 76 Error Event_key

--- Juego2.py
+++ Juego2.py
@@ -1,5 +1,4 @@
-
-import pygame, sys, glob, pytmx, Mapa, random
+import pygame, sys, glob, pytmx, Mapa, random, tmx
 from pygame.locals import*
 from pygame import*
 from pytmx.util_pygame import*
@@ -46,10 +45,10 @@ class player:
        self.anim = [0,0,0,0]
        
        
-       self.anim[0] = glob.glob("CaminandoAdelante/Caminando*.png")
-       self.anim[1] = glob.glob("CaminandoAtras/Caminando*.png")
-       self.anim[2] = glob.glob("CaminandoDerecha/Caminando*.png")
-       self.anim[3] = glob.glob("CaminandoIzquierda/Caminando*.png")
+       self.anim[0] = glob.glob("John/CaminandoAdelante/Caminando*.png")
+       self.anim[1] = glob.glob("John/CaminandoAtras/Caminando*.png")
+       self.anim[2] = glob.glob("John/CaminandoDerecha/Caminando*.png")
+       self.anim[3] = glob.glob("John/CaminandoIzquierda/Caminando*.png")
             
        self.anim[0].sort()
        self.anim[1].sort()
@@ -125,11 +124,13 @@ class Enemy:
 
 	       self.x1 = 300
 	       self.y1 = 300
-	       self.dir = 0              
+	       self.dir = 0             
+	       self.contador = 0
 	       self.initialAnimSpeed = 2
 	       self.currentAnimSpeed = self.initialAnimSpeed
-               
-          
+	       self.o = 0
+               #se cambiaria las imagenes
+	       self.a = True
 	       self.anim = [0,0,0,0]  
                
 	       self.anim[0] = glob.glob("Enemy/Perro/Adelante/Perro*.png")
@@ -154,61 +155,109 @@ class Enemy:
 	       self.rect.centery = 400
 	       self.updateIA(self.x1,self.y1)
 	def updateIA(self,playerx,playery):
+                self.contador += 1
                 self.currentAnimSpeed -= 1
                 distancia = ((self.x1 - (600 + playerx))**2 + (self.y1 - (300 + playery))**2)**(0.5)
                 if(distancia <= 300):
-                    		if((600 + playerx) != self.x1):
-                        		if((600 + playerx) - self.x1 < 0):
-                            			self.x1 = (self.x1 - 4)
-                            			self.image = load_image(self.anim[3][self.animPosition],True)
-                           			self.currentAnimSpeed = self.initialAnimSpeed
-                           			if self.animPosition == self.animMax2:
-                                			self.animPosition = 0
-                           			else:
-                                			self.animPosition += 1
-                            			#if(#aqui iria si no pudiera moverse):
+                    if((600 + playerx) > self.x1 + 5 or (600 + playerx) < self.x1 - 5):
+                        if((600 + playerx) - self.x1 < 0):
+                            self.x1 = (self.x1 - 4)
+                            self.image = load_image(self.anim[3][self.animPosition],True)
+                            self.currentAnimSpeed = self.initialAnimSpeed
+                            if self.animPosition == self.animMax2:
+                                self.animPosition = 0
+                            else:
+                                self.animPosition += 1
+                            #if(#aqui iria si no pudiera moverse):
 						#nose como sera la clase pared 3:
 						
-                        		if((600 + playerx) - self.x1 > 0):
-                            			self.x1 = (self.x1 + 4)
-                            			self.image = load_image(self.anim[2][self.animPosition],True)
-                            			self.currentAnimSpeed = self.initialAnimSpeed
-                            			if self.animPosition == self.animMax2:
-                                			self.animPosition = 0
-                            			else:
-                                			self.animPosition += 1
-                            			#if(#aqui iria si no pudiera moverse):            
-                    		if((300 + playery) != self.y1):
-                        		if((300 + playery) - self.y1 < 0):
-                           			self.y1 = (self.y1 - 4)
-                            			self.image = load_image(self.anim[1][self.animPosition],True)
-                            			self.currentAnimSpeed = self.initialAnimSpeed
-                            			if self.animPosition == self.animMax2:
-                                			self.animPosition = 0
-                            			else:
-                                			self.animPosition += 1
-                            			#if(#aqui iria si no pudiera moverse):
-                        		if((300 + playery) - self.y1 > 0):
-                            			self.y1 = (self.y1 + 4)
-                            			self.image = load_image(self.anim[0][self.animPosition],True)
-                            			self.currentAnimSpeed = self.initialAnimSpeed
-                            			if self.animPosition == self.animMax2:
-                                			self.animPosition = 0
-                            			else:
-                                			self.animPosition += 1
-                                		#if(#aqui iria si no pudiera moverse):
+                        if((600 + playerx) - self.x1 > 0):
+                            self.x1 = (self.x1 + 4)
+                            self.image = load_image(self.anim[2][self.animPosition],True)
+                            self.currentAnimSpeed = self.initialAnimSpeed
+                            if self.animPosition == self.animMax2:
+                                self.animPosition = 0
+                            else:
+                                self.animPosition += 1
+                            #if(#aqui iria si no pudiera moverse):
+                    else:            
+                        if((300 + playery) > self.y1 + 5 or (300 +  playery) < self.y1 - 5):
+                            if((300 + playery) - self.y1 < 0):
+                                self.y1 = (self.y1 - 4)
+                                self.image = load_image(self.anim[1][self.animPosition],True)
+                                self.currentAnimSpeed = self.initialAnimSpeed
+                                if self.animPosition == self.animMax2:
+                                    self.animPosition = 0
+                                else:
+                                    self.animPosition += 1
+                            #if(#aqui iria si no pudiera moverse):
+                            if((300 + playery) - self.y1 > 0):
+                                self.y1 = (self.y1 + 4)
+                                self.image = load_image(self.anim[0][self.animPosition],True)
+                                self.currentAnimSpeed = self.initialAnimSpeed
+                                if self.animPosition == self.animMax2:
+                                    self.animPosition = 0
+                                else:
+                                    self.animPosition += 1
+                                #if(#aqui iria si no pudiera moverse):
                 else:
-                    		o = random.randrange(4)
+                    if(self.a):
+                        self.o = random.randrange(4)
+                    o = self.o    
+                    if(o == 0):
+                        self.a = False
+                        self.x1 = self.x1 + 5
+                        self.image = load_image(self.anim[2][self.animPosition],True)
+                        self.currentAnimSpeed = self.initialAnimSpeed
+                        if self.animPosition == self.animMax2:
+                            self.animPosition = 0
+                        else:
+                            self.animPosition += 1
+                        if(self.contador == 50):    
+                            self.a = True
+                            self.contador = 0
                         
-                    		if(o == 0):
-                        		self.x1 = self.x1 + 5
-                    		if(o == 1):
-                        		self.x1 = self.x1 - 5
-                    		if(o == 2):
-                        		self.y1 = self.y1 + 5
-                    		if(o == 3):
-                        		self.y1 = self.y1 - 5
-					
+                    if(o == 1):
+                        self.a = False
+                        self.x1 = self.x1 - 5
+                        self.image = load_image(self.anim[3][self.animPosition],True)
+                        self.currentAnimSpeed = self.initialAnimSpeed
+                        if self.animPosition == self.animMax2:
+                            self.animPosition = 0
+                        else:
+                            self.animPosition += 1
+                        if(self.contador == 25):
+                            self.a = True
+                            self.contador = 0
+                    if(o == 2):
+                        self.a = False
+                        self.y1 = self.y1 + 5
+                        self.image = load_image(self.anim[0][self.animPosition],True)
+                        self.currentAnimSpeed = self.initialAnimSpeed
+                        if self.animPosition == self.animMax2:
+                            self.animPosition = 0
+                        else:
+                            self.animPosition += 1
+                        if(self.contador == 25):
+                            
+                            self.a = True
+                            self.contador = 0
+                        
+                    if(o == 3):
+                        self.a = False
+                        self.y1 = self.y1 - 5
+                        self.image = load_image(self.anim[1][self.animPosition],True)
+                        self.currentAnimSpeed = self.initialAnimSpeed
+                        if self.animPosition == self.animMax2:
+                            self.animPosition = 0
+                        else:
+                            self.animPosition += 1
+                        if(self.contador == 25):
+                            
+                            self.a = True
+                            self.contador = 0
+                        
+                
                 screen.blit(self.image,(self.x1 - playerx,self.y1 - playery)) 
 #class coin:
   #  def __init__(self):
@@ -219,262 +268,47 @@ class Enemy:
 
   #  def update(self):
    #     screen.blit(self.image,(self.x,self.y))
-class Medicina:
-	def __init__(self):
-		
-		self.anime = glob.glob("Medicina/Medicina*.png")
-		self.anime.sort()
-		self.z = 0
-		self.image = load_image(self.anime[self.z],True)
-		self.image2 = load_image("Medicina/Frasco.png",True)
-		self.rect1 = self.image.get_rect()
-		self.rect2 = self.image.get_rect()
-		
-		#posicion final del nivel
-		self.rect3 = self.image.get_rect()
-		self.rect3.centerx1 = 800
-		self.rect3.centery1 = 800
-		
-		self.cont1 = 1
-		self.cont2 = 2
-		self.cont3 = 3
-		self.cont4 = 4
-		
-		self.x = 0
-		self.y = 0
-		#se usa la funcion random para selecionar 2 posiciones sin repetirse
-		o = random.randrange(4)
-		if(o == 0):
-			self.x = self.cont1
-			o = random.randrange(3)
-			if(o == 0):
-				self.y = self.cont2
-			if(o == 1):
-				self.y = self.cont3
-			if(o == 2):
-				self.y = self.cont4
-		if(o == 1):
-			self.x = self.cont2
-			o = random.randrange(3)
-			if(o == 0):
-				self.y = self.cont1
-			if(o == 1):
-				self.y = self.cont3
-			if(o == 2):
-				self.y = self.cont4
-		if(o == 2):
-			self.x = self.cont3
-			o = random.randrange(3)
-			if(o == 0):
-				self.y = self.cont1
-			if(o == 1):
-				self.y = self.cont2
-			if(o == 2):
-				self.y = self.cont4
-		if(o == 3):
-			self.x = self.cont4
-			o = random.randrange(3)
-			if(o == 0):
-				self.y = self.cont1
-			if(o == 1):
-				self.y = self.cont2
-			if(o == 2):
-				self.y = self.cont3
-		#posiciones de la medicina dependiendo del random
-		#estas posiciones estan de ejemplo
-		if(self.x == 1):
-			self.x1 = self.rect1.centerx1 = 100
-			self.y1 = self.rect1.centery1 = 400
-		if(self.x == 2):
-			self.x1 =self.rect1.centerx2 = 200
-			self.y1 =self.rect1.centery2 = 400
-		if(self.x == 3):
-			self.x1 =self.rect1.centerx3 = 300
-			self.y1 =self.rect1.centery3 = 400
-		if(self.x == 4):
-			self.x1 =self.rect1.centerx4 = 400
-			self.y1 =self.rect1.centery4 = 400
-			
-		if(self.y == 1):
-			self.x2 = self.rect2.centerx1 = 100
-			self.y2 = self.rect2.centery1 = 400
-		if(self.y == 2):
-			self.x2 = self.rect2.centerx2 = 200
-			self.y2 =self.rect2.centery2 = 400
-		if(self.y == 3):
-			self.x2 = self.rect2.centerx3 = 300
-			self.y2 = self.rect2.centery3 = 400
-		if(self.y == 4):
-			self.x2 = self.rect2.centerx4 = 400
-			self.y2 = self.rect2.centery4 = 400
-		self.update()
-		def update(self):
-			#animacion de movimiento de la medicina
-			self.connt = 0
-			self.connt += 1
-			if(self.connt == 5):
-				self.connt = 0
-				self.z += 1
-				self.image = load_image(self.anime[self.z],True)
-			if(self.z == 7):
-				self.z = 0
-			
-		def TocarMedicina(self,playerx,playery,Medicinax,Medicinay,BarraCordura):
-			self.Variacionx = playerx - Medicinax
-			self.Variaciony = playery - Medicinay
-			
-			if(self.Variacionx < 3 and self.Variaciony < 3):
-				
-				self.z = self.z - 26
-				if(self.z < 0):
-					self.z = 0
-			
-					
-			#Sprite collide con las medicinas
-			#aumentar la vida en el momento que las toque(restar 28 a z de la vida)
-			#borrar la imagen de la medicina
-			
-			#final del nivel
-			
-				
-			
-		
-		
-class Vida:
-	def __init__(self,enemyx,enemyy):
-		self.anime = glob.glob("BarraCordura/BarraCordura*.png")
-		self.anime.sort()
-		self.z = 0
-		self.cont = 0
-		self.image = load_image(self.anime[self.z],True)
-       		self.rect = self.image.get_rect()
-       		self.rect.centerx = 100
-       		self.rect.centery = 500
-		self.animeMax = len(self.anime) - 1
-		self.gameover = False              
-		#colores para darle "un toque" al final
-		self.blanco = (255,255,255)
-		self.gris1 = (180,180,180)
-		self.gris2 = (100,100,100)
-		self.negro = (0,0,0)
-		self.updateVida(self.z,self.cont,enemyx,enemyy)
-	def ObjetoTexto(Mensaje,color):
-		textoSurperfice1 = font.render(Mensaje,True,color)
-		return textoSurperfice1 ,textoSurperfice1.get_rect()
-	def MensajePantalla(Mensaje , color , cambioy = 0):
-		textoSuperficie , textoRect = self.ObjetoTexto
-		textoRect.center = (w/2),(h/2) + cambioy
-		PantallaTexto = font.render(Mensaje , True , color)
-		screen.blit(textoSuperficie,textoRect)
-	def updateVida(self,playerx,playery,Enemyx1,Enemyy1):
-		self.Variacionx = (playerx - Enemyx1)**2
-		self.Variaciony = (playery - Enemyy1)**2
-		self.cont+=1
-		if(self.cont == 600):
-				
-			self.z = self.z + 1
-			self.image = load_image(self.anime[self.z],True)
-			self.cont = 0
-			
-		if(self.Variacionx < 9 and self.Variaciony < 9):
-				
-			self.z = self.z + 26
-			self.cont1 = 1
-			self.cont2 = 0
-			if(self.z > 78):
-				self.z = 78
-			self.image = load_image(self.anime[self.z],True)
-				
-			while(self.cont1 == 1):
-				self.cont2 = self.cont2 + 1
-				if(self.cont2 == 5):
-					self.cont1 = 0
-					self.cont2 = 0
-			if(self.z == 78):
-				#"mensaje de game over y opciones para continuar o salir"
-				self.gameover = True
-				self.contDie = 0
-				screen.fill(self.gris2)
-				pygame.display.update()
-				
-			while self.gameover == True:
-				self.contDie+=1
-				if(self.contDie == 1):
-					screen.fill(self.negro)
-				
-				if(self.contDie == 4):
-					screen.fill(self.gris1)
-				if(self.contDie == 7):
-					screen.fill(self.blanco)
-				if(self.contDie == 8):
-					self.MensajePantalla("Has perdido" , self.gris1,20)
-				if(self.contDie == 9):
-					self.MensajePantalla("Has perdido" , self.gris2,20)
-				if(self.contDie == 10):
-					self.MensajePantalla("Has perdido" , self.negro,20)
-					self.MensajePantalla("ESPACIO para continuar y ESCAPE para salir" , self.negro , 0)
-				
-				
-					
-				pygame.display.update()
-					
-				for event in pygame.event.get():
-					if event.type == pygame.QUIT:
-						pygame.quit()
-            					sys.exit()
-					if event.type == pygame.KEYDOWN:
-						if event.key == pygame.K_SPACE:
-							self.gameover = False
-							Juego()
-						if event.key == pygame.K_ESCAPE:
-							menu1()
-				
-					
-				
-		
+
+
 
 
 #coin1 = coin()
-def Juego():
-	
-	player1 = player()
-	enemy1 = Enemy()
-	vida1 = Vida(enemy1.x1,enemy1.y1)
-	position = 0
-	pygame.mixer.init()
-	pygame.mixer.music.load("ice_zone.mp3")
-	pygame.mixer.music.play(-1,0.0)
+player1 = player()
+enemy1 = Enemy()
+position = 0
+
+pygame.mixer.init()
+pygame.mixer.music.load("ice_zone.mp3")
+pygame.mixer.music.play(-1,0.0)
 
 
 
-	while 1:
-	    screen.fill((0,0,0))
-	    clock.tick(30)
-	    #if player1.y == coin1.y:
-		#effect = pygame.mixer.Sound("coin.ogg")
-		#effect.play()
-		#if coin1.y == 350:
-		#    coin1.y = 250
-		#elif coin1.y == 250:
-		#    coin1.y = 350
 
-	    #else:
-	    #    coin1.update()    
-	    for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-		    pygame.quit()
-		    sys.exit()
-		elif event.type == KEYDOWN and (event.key == K_UP or event.key == K_DOWN or event.key == K_LEFT or event.key == K_RIGHT):
-		    position = 6
+while 1:
+    screen.fill((0,0,0))
+    clock.tick(30)
+    #if player1.y == coin1.y:
+        #effect = pygame.mixer.Sound("coin.ogg")
+        #effect.play()
+        #if coin1.y == 350:
+        #    coin1.y = 250
+        #elif coin1.y == 250:
+        #    coin1.y = 350
+   
+    #else:
+    #    coin1.update()    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYDOWN and (event.key == K_UP or event.key == K_DOWN or event.key == K_LEFT or event.key == K_RIGHT):
+            position = 6
+            
+        elif event.type == KEYUP and (event.key == K_UP or event.key == K_DOWN or event.key == K_LEFT or event.key == K_RIGHT):
+            position = 0
+            player1.animPosition = 0
 
-		elif event.type == KEYUP and (event.key == K_UP or event.key == K_DOWN or event.key == K_LEFT or event.key == K_RIGHT):
-		    position = 0
-		    player1.animPosition = 0
-
-	    Mapa.render_tiles_to_screen("Mapa1.tmx",player1.x,player1.y, player1.rect) 
-      
-	    enemy1.updateIA(player1.x,player1.y)
-	    player1.update(position)
-	    vida1.updateVida(player1.x,player1.y,enemy1.x1,enemy1.y1)
-	    pygame.display.flip()       
-Juego()
+    Mapa.render_tiles_to_screen("Mapa1.tmx",player1.x,player1.y, player1.rect, player1.dir) 
+    enemy1.updateIA(player1.x,player1.y)
+    player1.update(position)
+    pygame.display.flip()       
